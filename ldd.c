3e893b30910d710961ba931a8fde4c84580a777b
Jose <jose.figueroa@assaabloy.com>
2025-10-08 09:46:01 -0600

    feat: Add proc_fs. Add read function

--- ldd.c
+++ ldd.c
@@ -10,34 +10,66 @@
  * 
  */
 
-#include <linux/init.h>      // Needed for module_init and module_exit macros
-#include <linux/module.h>    // Needed for all kernel modules
+#include <linux/init.h>     // Needed for module_init and module_exit macros
+#include <linux/module.h>   // Needed for all kernel modules
+#include <proc_fs.h>        // Needed for proc filesystem operations
 
 MODULE_LICENSE("GPL"); // License type -- affects kernel behavior
 MODULE_AUTHOR("Jose Figueroa"); // Module author
 MODULE_DESCRIPTION("A simple example kernel module"); // Short description
 
+static struct proc_dir_entry *custom_proc_node;
+
+/**
+ * @brief Read function for the proc file.
+ * This function is called when the proc file is read.
+ * It currently just prints a message to the kernel log.
+ * 
+ * @param file_ptr Pointer to the file structure.
+ * @param user_space_buffer Buffer to store data read from the proc file.
+ * @param count Number of bytes to read.
+ * @param offset Offset in the file.
+ * @return ssize_t Number of bytes read or negative error code.
+ */
+static ssize_t ldd_read(struct file *file_ptr, char *user_space_buffer, size_t count, loff_t *offset)
+{
+    printk("ldd_read \n");
+    return 0;
+}
+
+struct proc_ops driver_proc_ops = 
+{
+    .proc_read = ldd_read
+};
+
 /**
  * @brief Module initialization function.
  * This function is called when the module is loaded into the kernel.
- * It prints a hello message to the kernel log.
+ * It prints stage messages to the kernel log and creates a proc entry.
  * 
  * @return int Returns 0 on success. Non-zero means module can't be loaded.
  */
 static int ldd_init(void)
 {
-    printk("Hello, World! from Jose\n"); // Print message to kernel log
+    printk("ldd_init: entry\n"); 
+    // Create a proc entry. 
+    custom_proc_node = proc_create("ldd_Driver", 0, NULL, &driver_proc_ops); 
+    printk("ldd_init: exit\n"); 
+
     return 0; // Return 0 for successful loading
 }
 
 /**
  * @brief Module cleanup function.
  * This function is called just before the module is removed from the kernel.
- * It prints a goodbye message to the kernel log.
+ * It prints stage messages to the kernel log and removes the proc entry.
  */
 static void ldd_exit(void)
 {
-    printk("Goodbye, World! from Jose\n"); // Print message to kernel log
+    printk("ldd_exit: entry\n"); 
+    // Remove the proc entry.
+    proc_remove(custom_proc_node); 
+    printk("ldd_exit: exit\n"); 
 }
 
 /* Macros to register module entry and exit points */
